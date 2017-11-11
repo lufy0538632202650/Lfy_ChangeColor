b262cbe7a74f40a7be0a389f5c505cf27631b75a
lufy0538632202650 <lu.fy@biw.net.cn>
2017-11-11 11:58:42 +0800

    sfa
    saasdfas

---
+++ Lfy_ChangeColor/ChangeColorCtl.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace Lfy_ChangeColor
+{
+    partial class Ctl_ColorChange
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region 组件设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+			this.Ctl_Lbl = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// Ctl_Lbl
+			// 
+			this.Ctl_Lbl.AutoSize = true;
+			this.Ctl_Lbl.Location = new System.Drawing.Point(4, 4);
+			this.Ctl_Lbl.Name = "Ctl_Lbl";
+			this.Ctl_Lbl.Size = new System.Drawing.Size(0, 12);
+			this.Ctl_Lbl.TabIndex = 0;
+			// 
+			// Ctl_ColorChange
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.Red;
+			this.Controls.Add(this.Ctl_Lbl);
+			this.Name = "Ctl_ColorChange";
+			this.Size = new System.Drawing.Size(40, 40);
+			this.Load += new System.EventHandler(this.Ctl_ColorChange_Load);
+			this.Click += new System.EventHandler(this.ColorChange_Click);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+        }
+
+		#endregion
+
+		private System.Windows.Forms.Label Ctl_Lbl;
+	}
+}
---
+++ Lfy_ChangeColor/ChangeColorCtl.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lfy_ChangeColor
+{
+    public partial class Ctl_ColorChange: UserControl
+    {		
+        public Ctl_ColorChange()
+        {
+            InitializeComponent();
+        }
+		public Ctl_ColorChange(string str)
+		{
+			
+			InitializeComponent();
+			this.Ctl_Lbl.Text = str;
+		}
+		
+	     private void ColorChange_Click(object sender, EventArgs e)
+		{
+			if (((Ctl_ColorChange)sender).BackColor == Color.Red)
+			{ ((Ctl_ColorChange)sender).BackColor = Color.Yellow; }
+			else if (((Ctl_ColorChange)sender).BackColor == Color.Yellow)
+			{ ((Ctl_ColorChange)sender).BackColor = Color.Green; }
+			else
+			{ ((Ctl_ColorChange)sender).BackColor = Color.Red; }
+		}
+
+		private void Ctl_ColorChange_Load(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
